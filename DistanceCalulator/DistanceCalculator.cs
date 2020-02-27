8994f9e4b6449afe021381561e9bb34114479c64
Joachim Nordeke <joachim.nordeke@starrepublic.com>
2020-02-27 15:32:12 +0100

    Add project files.

---
+++ DistanceCalulator/DistanceCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DistanceCalulator
+{
+    public static class DistanceCalculator
+    {
+        private const double earthRadiusKm = 6371;
+        private const double earthRadiusMi = 3958.8;
+
+
+        public static double GetKilometers(Position pos1, Position pos2) =>
+            Math.Round(earthRadiusKm * GetDistance(pos1, pos2), 3);
+
+        public static double GetMiles(Position pos1, Position pos2) =>
+            Math.Round(earthRadiusMi * GetDistance(pos1, pos2), 3);
+
+
+        private static double GetDistance(Position pos1, Position pos2)
+        {
+            // Calculation based on https://andrew.hedges.name/experiments/haversine/ (2020-02-27)
+
+            double dLon = DegreesToRadius(pos2.Longitude - pos1.Longitude);
+            double dLat = DegreesToRadius(pos2.Latitude - pos1.Latitude);
+
+            var a =
+                Math.Pow(Math.Sin(dLat / 2), 2) +
+                Math.Cos(DegreesToRadius(pos1.Latitude)) *
+                Math.Cos(DegreesToRadius(pos2.Latitude)) *
+                Math.Pow(Math.Sin(dLon / 2), 2);
+
+            return 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+        }
+
+        private static double DegreesToRadius(double deg) =>
+            deg * (Math.PI / 180);
+    }
+}
---
+++ DistanceCalulator/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace DistanceCalulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Distance Calculator");
+            Console.WriteLine("-------------------");
+
+            var pos1 = new Position();
+            Console.Write("Latitude 1: ");
+            pos1.Latitude = ReadDouble();
+            Console.Write("Longitude 1: ");
+            pos1.Longitude = ReadDouble();
+
+            var pos2 = new Position();
+            Console.Write("Latitude 2: ");
+            pos2.Latitude = ReadDouble();
+            Console.Write("Longitude 2: ");
+            pos2.Longitude = ReadDouble();
+
+            Console.WriteLine("-------------------");
+
+            var miles = DistanceCalculator.GetMiles(pos1, pos2);
+            var kilometers = DistanceCalculator.GetKilometers(pos1, pos2);
+
+            Console.WriteLine($"{miles} miles");
+            Console.WriteLine($"{kilometers} km");
+
+            Console.ReadLine();
+        }
+
+        static double ReadDouble()
+        {
+            double? result = null;
+
+            while (result == null)
+            {
+                string value = Console.ReadLine();
+
+                try
+                {
+                    value = value.Replace('.', ',').Trim();
+                    result = double.Parse(value);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.Write("Try again: ");
+                }
+            }
+
+            return result.Value;
+        }
+    }
+}
